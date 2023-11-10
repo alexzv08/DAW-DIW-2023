b9da5b17f3e089539987d346c8467c53f5e97304
alexzv08 <alexzv8@gmail.com>
2023-11-10 17:14:56 +0100

    Curso animaciones terminado

--- cursoAnimaciones/js/animacion.js
+++ cursoAnimaciones/js/animacion.js
@@ -2,5 +2,16 @@
 
 window.onload = function load(){
     let chincheta=document.getElementsByClassName("chincheta")[0];
-    chincheta.classList.add("chincheta1");    
+    chincheta.classList.add("chincheta1");
+    let chincheta1=document.getElementsByClassName("chincheta")[1];
+    chincheta1.classList.add("chincheta2");
+    let chincheta2=document.getElementsByClassName("chincheta")[2];
+    chincheta2.classList.add("chincheta3");
+    
+    let art1=document.getElementById("art1");
+    art1.classList.add("art1");
+    
+    let art2=document.getElementById("art2");
+    art2.classList.add("art2");
+    
 }
